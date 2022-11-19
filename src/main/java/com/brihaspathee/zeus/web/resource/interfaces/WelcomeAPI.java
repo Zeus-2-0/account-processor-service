4910b9040da984b231aaf25ed8277b244b29d364
vbalaji215-dev <vbalaji215@outlook.com>
2022-11-18 21:42:52 -0500

    ZEUS-228 created Welcome api

---
+++ src/main/java/com/brihaspathee/zeus/web/resource/interfaces/WelcomeAPI.java
@@ -0,0 +1,10 @@
+package com.brihaspathee.zeus.web.resource.interfaces;/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 18, November 2022
+ * Time: 4:19 PM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.web.resource.interfaces
+ * To change this template use File | Settings | File and Code Template
+ */public class WelcomeAPI {
+}
